a24a514195f0e28b56935f8e323052f7a112172d
tmdgh5623 <tmdgh5623@gmail.com>
2025-06-04 17:09:29 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyBmKazNVKE4dEF7bkEsbBAYhRTQp94x4fo",
+  authDomain: "team-calendar-6396c.firebaseapp.com",
+  projectId: "team-calendar-6396c",
+  storageBucket: "team-calendar-6396c.firebasestorage.app",
+  messagingSenderId: "258023333622",
+  appId: "1:258023333622:web:90c98ec2cb49d0723f96d7",
+  measurementId: "G-GXMSRX3CXT"
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
