e4f95a10d1082bfd3bcf10d7c79b30578f5056cd
tmdgh5623 <tmdgh5623@gmail.com>
2025-06-09 17:29:36 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "index.html" // ✅ 경로에서 "public/" 제거
-    }
-  }
+  plugins: [react()]
 });
