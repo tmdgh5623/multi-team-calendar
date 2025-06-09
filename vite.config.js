dbab7417b9e4c0cfde511700896af78eca3b3734
tmdgh5623 <tmdgh5623@gmail.com>
2025-06-09 17:25:20 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".",
   build: {
-    outDir: "dist",
     rollupOptions: {
-      input: "public/index.html"
+      input: "index.html" // ✅ 경로에서 "public/" 제거
     }
   }
 });
