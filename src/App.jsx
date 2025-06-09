d5b635254966cf2e9a6c42ac6486b2c50c13bf5d
tmdgh5623 <tmdgh5623@gmail.com>
2025-06-09 11:07:46 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import Dashboard from './pages/Dashboard';
+import SummaryList from './pages/SummaryList';
+import CalendarPage from './pages/CalendarPage';
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/summary" element={<SummaryList />} />
+        <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
     </BrowserRouter>
   );
